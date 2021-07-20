1b84a14cc4ca0f3214fa874faca8f68a7360a495
Mohamed Ahmed Abdelkader <madsaaeq@gmail.com>
2021-07-20 22:01:50 +0200

    Setting up kubernetes.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,16 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="mohamedaae/mohamed-devops-project"
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run mohamed-devops --image=$dockerpath --port=80
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
-
+kubectl port-forward mohamed-devops 8000:80
+kubectl logs `kubectl get pods -o=name`
