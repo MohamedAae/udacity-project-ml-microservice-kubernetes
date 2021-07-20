7aedc9cc34dac5e7f75fe115197fa14b3e8761b4
Mohamed Ahmed Abdelkader <madsaaeq@gmail.com>
2021-07-20 20:44:43 +0200

    Script uploading to docker hub.

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -3,13 +3,14 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="mohamedaae/mohamed-devops-project"
 
-# Step 2:  
 # Authenticate & tag
+docker login --username mohamedaae
+docker tag mohamed-devops-project $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
+docker push $dockerpath
